9e9ba9411129d2d4bbbfdcfde61de36365b400fe
uoFishbox <cfncxlanota@gmail.com>
2025-10-04 22:25:47 +0900

    refactor: remove EventManager and integrate event handling directly in AnotherTwoHopLinksPlugin

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,52 @@
 import { Plugin, MarkdownView, WorkspaceLeaf, TFile } from "obsidian";
 import { ComponentManager } from "./ComponentManager";
-import { EventManager } from "./EventManager";
 import { ViewManager } from "./ViewManager";
 
 export default class AnotherTwoHopLinksPlugin extends Plugin {
 	private componentManager: ComponentManager = new ComponentManager();
 	private viewManager!: ViewManager;
-	private eventManager!: EventManager;
 
 	async onload() {
 		console.log("Loading Example Plugin");
 
 		this.viewManager = new ViewManager(this.app);
 
-		this.eventManager = new EventManager(this, {
-			onLayoutReady: () => {
-				const leaf =
-					this.app.workspace.getActiveViewOfType(MarkdownView)
-						?.leaf || null;
-				const file = this.viewManager.handleActiveLeafChange(leaf);
-				if (leaf && file && leaf.view instanceof MarkdownView) {
-					this.componentManager.mountComponentsForView(
-						leaf.view as MarkdownView,
-						file
-					);
-				}
-			},
-			onActiveLeafChange: (leaf) => {
-				const file = this.viewManager.handleActiveLeafChange(leaf);
-				if (leaf && file && leaf.view instanceof MarkdownView) {
-					this.componentManager.mountComponentsForView(
-						leaf.view as MarkdownView,
-						file
-					);
-				}
-			},
-			onLayoutChange: () => {
-				const activeViews = this.viewManager.getActiveMarkdownViews();
-				this.componentManager.cleanupClosedViews(activeViews);
-			},
+		// Obsidianのレイアウト準備完了後に、最初にアクティブなビューにマウント
+		this.app.workspace.onLayoutReady(() => {
+			const leaf =
+				this.app.workspace.getActiveViewOfType(MarkdownView)?.leaf ||
+				null;
+			const file = this.viewManager.handleActiveLeafChange(leaf);
+			if (leaf && file && leaf.view instanceof MarkdownView) {
+				this.componentManager.mountComponentsForView(
+					leaf.view as MarkdownView,
+					file
+				);
+			}
 		});
 
-		this.eventManager.registerEvents();
+		// アクティブなLeafが変更されたときにコンポーネントをマウント/更新
+		this.registerEvent(
+			this.app.workspace.on(
+				"active-leaf-change",
+				(leaf: WorkspaceLeaf | null) => {
+					const file = this.viewManager.handleActiveLeafChange(leaf);
+					if (leaf && file && leaf.view instanceof MarkdownView) {
+						this.componentManager.mountComponentsForView(
+							leaf.view as MarkdownView,
+							file
+						);
+					}
+				}
+			)
+		);
+
+		this.registerEvent(
+			this.app.workspace.on("layout-change", () => {
+				const activeViews = this.viewManager.getActiveMarkdownViews();
+				this.componentManager.cleanupClosedViews(activeViews);
+			})
+		);
 	}
 
 	onunload() {
